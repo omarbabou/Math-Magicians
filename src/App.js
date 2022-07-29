6841a0d7b7265d5e2eb64e308507cae706e51609
omarbabou <obabou50@gmail.com>
2022-07-29 11:06:32 +0200

    Add calculator components

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Calculator from './components/calculator';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Calculator />
+      </div>
+    );
+  }
+}
+export default App;
